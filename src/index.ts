7b553524d009af6cd2d36cc2f20bdc4da88fb74a
William Lahti <wilahti@gmail.com>
2024-01-08 18:21:47 -0800

    add missing export: ServiceType

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,6 @@ export * from './ServiceType';
 export * from './MulticastDNS';
 export * from './ResourceRecord';
 export * from './QueryRecord';
+export * from './ServiceType';
+
 export * as validate from './validate';
